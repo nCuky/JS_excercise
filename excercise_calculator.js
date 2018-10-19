bdb8a634589c5403449c69eb7c82c5e415ff3d49
Cuky <shvertzen@gmail.com>
2018-10-19 18:08:52 +0300

    working

--- excercise_calculator.js
+++ excercise_calculator.js
@@ -1,124 +1,128 @@
 // Excercise: Calculator
 
-let calculatorMatrix = new Array[10];
+let calculatorMatrix = [];
 
 const cAdd = "+",
-      cSubtract = "-",
-      cMultiply = "*",
-      cDivide = "/",
-      cModulus = "%",
-      cPower = "^",
-      cEquals = "=";
+  cSubtract = "-",
+  cMultiply = "*",
+  cDivide = "/",
+  cModulus = "%",
+  cPower = "^",
+  cEquals = "=";
 
-let operand1,
-operator,
-operand2;
+let operand1, operator, operand2;
 whichOperand = 0;
-inputTextBoxValue;
 
-function initializeMatrix(){
-for (let i = 0; i < Array.length(calculatorMatrix); i++){
-calculatorMatrix[i] = i;
-
+function initializeMatrix() {
+  for (let i = 0; i <= 9; i++) {
+    calculatorMatrix[i] = i;
+  }
 }
 
+function setOperator(_operator) {
+  operator = _operator;
+  document.getElementById("operator").innerHTML = operator;
 }
 
-function setOperator(_operator)
-{
-    operator = _operator;
+function setWhichOperand(_whichOperand) {
+  whichOperand = _whichOperand;
 }
 
-function setWhichOperand(_whichOperand)
-{
-    whichOperand = _whichOperand;
+function setEnteredOperand1(number) {
+  operand1 = number;
+  document.getElementById("operand1").innerHTML = operand1;
 }
 
-function setEnteredOperand1(number)
-{
-operand1 = number;
+function setEnteredOperand2(number) {
+  operand2 = number;
 }
 
-function setEnteredOperand2(number)
-{
-operand2 = number;
-}
+function calculateResult() {
+  debugger;
 
-function calculateResult()
-{
-    let result;
+  let result,
+    intOperand1 = parseInt(operand1),
+    intOperand2 = parseInt(operand2);
 
-    switch (operation)
-    {
-        case cAdd:
-        {
-            result = operand1 + operand2;
-        }
-        case cSubtract:
-        {
-            result = operand1 - operand2;
-        }
-        case cMultiply:
-        {
-            result = operand1 * operand2;
-        }
-        case cDivide:
-        {
-            if (operand2 != 0)
-            {
-                result = operand1 / operand2;
-            }
-            else{
-                result = 0;
-            }
-        }
-        case cEquals:
-        {
-            result = operand1;
-        }
+  switch (operator) {
+    case cAdd: {
+      result = intOperand1 + intOperand2;
+      break;
     }
+    case cSubtract: {
+      result = intOperand1 - intOperand2;
+      break;
+    }
+    case cMultiply: {
+      result = intOperand1 * intOperand2;
+      break;
+    }
+    case cDivide: {
+      if (operand2 != 0) {
+        result = intOperand1 / intOperand2;
+      } else {
+        result = 0;
+      }
+      break;
+    }
+    case cEquals: {
+      result = intOperand1;
+      debugger;
+      setEnteredOperand1(null);
+      setOperator(null);
 
-    return result;
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+
+  document.getElementById("inputArea").value = null;
+  document.getElementById("resultArea").value = result;
+
+  return result;
 }
 
-function eventClick(input)
-{
-    let inputType = typeof(input);
+function eventClick(input) {
+  debugger;
 
-    switch (inputType)
-    {
-        case "string":
-        {
-            if (operand1 == undefined)
-            {
-                setEnteredOperand1(inputTextBoxValue);
-                setOperator(input);
-                setWhichOperand(1);
-            }
-            else{
-                setEnteredOperand2(inputTextBoxValue);
-                setEnteredOperand1(calculateResult());            
-                inputTextBoxValue = operand1;
+  let inputType = typeof input;
 
-                setEnteredOperand2(undefined);
+  switch (inputType) {
+    case "string": {
+      if (operand1 == undefined) {
+        setEnteredOperand1(document.getElementById("inputArea").value);
+        setOperator(input);
+        setWhichOperand(1);
+      } else {
+        setEnteredOperand2(document.getElementById("inputArea").value);
+        setEnteredOperand1(calculateResult());
 
-                setOperator(input);
-                            setWhichOperand(1);
-            }
-        }
-        case "number":
-        {
-            inputTextBoxValue &= input;
-            
-            switch (whichOperand){
-                case 0:
-                {
-            setEnteredOperand1(inputTextBoxValue);
-                }
-                case 1:
-                {
-                setEnteredOperand2(inputTextBoxValue);
-            }
-        }
+        setEnteredOperand2(undefined);
+
+        setOperator(input);
+        setWhichOperand(1);
+      }
+
+      document.getElementById("inputArea").value = null;
+
+      break;
     }
+    case "number": {
+      document.getElementById("inputArea").value &= input;
+
+      switch (whichOperand) {
+        case 0: {
+          setEnteredOperand1(document.getElementById("inputArea").value);
+          break;
+        }
+        case 1: {
+          setEnteredOperand2(document.getElementById("inputArea").value);
+          break;
+        }
+      }
+      break;
+    }
+  }
 }
